deb51cc02841e753c3d49a231899a2ae40fcd2e5
Dani <danielpescadormateo@gmail.com>
2015-03-16 16:33:31 +0100

    MOdificado datatables

---
+++ app/php/mysql.php
@@ -0,0 +1,7 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$gaSql['user'] = "pablocacho_a";
+$gaSql['password'] = "aa12345";
+$gaSql['db'] = "pablocacho_clinicas";
+$gaSql['server'] = "localhost";
+?>
